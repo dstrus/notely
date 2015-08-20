02e0c81644d9ba580abf34fdf5b150f0415c5f4c
Dave Strus <dave@davestrus.com>
2015-08-20 15:41:02 -0400

    Offload API calls to NotesBackend module.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -3,28 +3,25 @@
 var nevernoteBasePath = 'https://nevernote-1150.herokuapp.com/api/v1/',
     apiKey = '$2a$10$TTc8gLTzfWBk9SsDO7p.J.acOzMMG535814CudrCMQgmjUSvbQ2ju';
 
-angular.module('notely.notes', ['ngRoute'])
+var noteApp = angular.module('notely.notes', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
+noteApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/notes', {
     templateUrl: 'notes/notes.html'
   });
-}])
+}]);
 
-.controller('NotesController', ['$scope', '$http', function($scope, $http) {
+noteApp.controller('NotesController', ['$scope', 'NotesBackend', function($scope, NotesBackend) {
+  var self = this;
   $scope.note = {};
 
-  $http.get(nevernoteBasePath + 'notes?api_key=' + apiKey)
-    .success(function(notesData) {
-      $scope.notes = notesData;
-    });
+  self.assignNotes = function(notes) {
+    $scope.notes = notes;
+  };
 
   $scope.commit = function() {
-    $http.post(nevernoteBasePath + 'notes', {
-      api_key: apiKey,
-      note: $scope.note
-    }).success(function(newNoteData) {
-      $scope.notes.unshift(newNoteData.note);
-    });
+    NotesBackend.postNote($scope.note, self.assignNotes);
   };
+
+  NotesBackend.fetchNotes(self.assignNotes);
 }]);
